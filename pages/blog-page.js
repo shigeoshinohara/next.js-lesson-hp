0bde6aeb90e3d5cb766c6d12b57926c171fb042d
ShigeoShinohara <s.shinohara@sync-web.jp>
2021-03-28 08:18:42 +0900

    change to function

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,31 +1,6 @@
-/*
 import Layout from "../components/Layout";
 import { getAllPostsData } from "../lib/posts";
 import Post from "../components/Post";
-
-const Blog = ({ posts }) => {
-  return (
-    <Layout title="Blog">
-      <ul className="m-10">
-        {posts && posts.map((post) => <Post key={post.id} post={post} />)}
-      </ul>
-    </Layout>
-  );
-};
-
-export default Blog;
-
-export async function getStaticProps() {
-  const posts = await getAllPostsData();
-  return {
-    props: { posts },
-  };
-}
-
-*/
-import Layout from "../components/Layout";
-import Post from "../components/Post";
-import { getAllPostsData } from "../lib/posts";
 
 export default function Blog({ posts }) {
   return (
